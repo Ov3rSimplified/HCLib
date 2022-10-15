2e89ddef2c2aef94eb1d6c0db5383ba49c726eb7
OV3rSimplified <81756768+Ov3rSimplified@users.noreply.github.com>
2022-10-15 19:25:04 +0200

    Update fra.lua
    French language update

--- hclib/lua/hclib/scripts/hclib/setup/languages/fra.lua
+++ hclib/lua/hclib/scripts/hclib/setup/languages/fra.lua
@@ -1,6 +1,6 @@
 return {
     ["ManageModules"] = "Gérer les modules",
-
+        
     ["Home"] = "Global",
 
     ["DangerZone"] = "Zone de Danger",
@@ -26,4 +26,42 @@ return {
     ["search"] = "recherche...",
 
     ["vgui.binder.pressakey"] = "Appuie sur une touche",
+
+    ["maincfg.MySQL"] = "Configuration MySQl",
+
+    ["maincfg.MySQL.Use"] = "Activer MySQL",
+
+    ["maincfg.MySQL.IP"] = "Adresse IP",
+
+    ["maincfg.MySQL.DBName"] = "Nom de la base de données",
+
+    ["maincfg.MySQL.Username"] = "Nom Utilisateur",
+
+    ["maincfg.MySQL.Password"] = "Mots de passe",
+
+    ["maincfg.MySQL.Port"] = "Port",
+
+    ["maincfg.Ranks"] = "Configuration Groupe",
+
+    ["maincfg.Ranks.Usergroups"] = "Groupe utilisateur",
+
+    ["maincfg.Ranks.Permissions"] = "Permissions",
+
+    ["maincfg.Ranks.AddUsergroups"] = "Ajouter un groupe",
+
+    ["maincfg.Ranks.EnterUsergroupname"] = "Indiquer le nom du groupe utilisateur",
+
+    ["maincfg.Ranks.Usergroupname.InfoTitle"] = "Nom du groupe utilisateur",
+
+    ["maincfg.Ranks.Usergroupname.Info"] = "merci, d'être sure que vous avez bien écrit le nom du groupe utilisateur correctement!",
+
+    ["maincfg.Ranks.selectside.Info"] = "Gauche, choissire; Droite, Menu pour supprimer le groupe!",
+
+    ["maincfg.Ranks.permissionside.Info"] = "GAUCHE, Menu pour supprimer les permissions!",
+
+    ["maincfg.Delete"] = "Supprimer",
+
+    ["Modules.Activate"] = "Activer",
+    
+    ["Modules.Disable"] = "Desactiver", 
 };
